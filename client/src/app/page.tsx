e97310d340d1c6ac4d3fc7689c82fb049bc8cd18
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-08-14 15:31:38 +0300

    feat: error handling, toast messages, styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -17,6 +17,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 const fetcher = (url: string) =>
   fetch(`http://localhost:4000/${url}`).then((res) => {
@@ -27,14 +28,25 @@ const fetcher = (url: string) =>
   });
 
 export default function Home() {
-  const [ email, setEmail ] = useState<string>("")
+  const { toast } = useToast();
+  const [email, setEmail] = useState<string>("");
   // const [ subscriptions, setSubscriptions ] = useState<string>('')
 
   // Registers every website visit
-  const { } = useSWR("api/track-visit", fetcher);
+  // const {} = useSWR("api/track-visit", fetcher);
 
   const handleSubmit = async (email: string) => {
     try {
+      if (!validateEmail(email)) {
+        toast({
+          title: "Error: Invalid email address",
+          description:
+            "Please make sure you email address is valid and try again.",
+          duration: 3000,
+        });
+        return;
+      }
+
       const response = await fetch(`http://localhost:4000/api/subscribe`, {
         method: "POST",
         headers: {
@@ -46,75 +58,136 @@ export default function Home() {
         throw new Error("Network response was not ok");
       }
       const data = await response.text();
-      // setSubscriptions(data)
+
+      toast({
+        title: "Subscription successful!",
+        description:
+          "You will now recieve the UO Dining Hall menu everyday at 6am.",
+        duration: 3000,
+      });
+
+      setEmail("");
     } catch (error) {
-      console.error("Error:", error);
+      toast({
+        title: "Error: Could not subscribe",
+        description: "Please try again.",
+        duration: 3000,
+      });
+
+      setEmail("");
     }
   };
 
+  const validateEmail = (email: string): boolean => {
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Check if the email matches the regex pattern
+    if (!emailRegex.test(email)) {
+      return false;
+    }
+
+    // Check if the email length is within the acceptable range
+    if (email.length > 254) {
+      return false;
+    }
+
+    // Check if the local part (before @) and domain part (after @) are within acceptable lengths
+    const [localPart, domainPart] = email.split("@");
+    if (localPart.length > 64 || domainPart.length > 255) {
+      return false;
+    }
+
+    // Ensure the domain part contains at least one dot and the top-level domain is valid
+    const domainParts = domainPart.split(".");
+    if (
+      domainParts.length < 2 ||
+      domainParts.some((part) => part.length === 0)
+    ) {
+      return false;
+    }
+
+    // Basic check to see if the top-level domain has at least two characters
+    const topLevelDomain = domainParts[domainParts.length - 1];
+    if (topLevelDomain.length < 2) {
+      return false;
+    }
+
+    return true;
+  };
+
   return (
-    <main>
-      {/* <div
-        className={`flex justify-center items-center flex-col m-2 ${cn(
-          "text-[0.8rem] text-muted-foreground"
-        )}`}
-      >
-        <p>Subscriptions: {subscriptions}</p>
-      </div> */}
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Email</TabsTrigger>
-          <TabsTrigger value="password">SMS</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Subscribe with Email</CardTitle>
-              <CardDescription>
-                Subscribe to recieve uOttawa&apos;s Dining Hall menu everyday by
-                email at 6:00am.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" placeholder="user@uottawa.ca" onChange={(e) => setEmail(e.target.value)}/>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button onClick={() => handleSubmit(email)}>Subscribe with email</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Subscribe wth SMS</CardTitle>
-              <CardDescription>
-                Subscribe to recieve uOttawa&apos;s Dining Hall menu everyday by SMS
-                at 6:00am.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="phone">Phone Number</Label>
-                <Input id="phone" type="phone" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button variant="secondary" disabled={true}>
-                Coming soon...
-              </Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
-      <div className={`m-2 ${cn("text-[0.8rem] text-muted-foreground")}`}>
-        Want to unsubscribe? Click{" "}
-        <Link href="/unsubscribe" className="underline">
-          here
-        </Link>.
-      </div>
-    </main>
+    <>
+      <main className="flex flex-col justify-center items-center gap-y-5">
+        <h1 className="text-[4rem] text-[#8F001A] font-bold ">
+          UO Dining Hall
+        </h1>
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="account">Email</TabsTrigger>
+            <TabsTrigger value="password">SMS</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <Card>
+              <CardHeader>
+                <CardTitle>Subscribe with Email</CardTitle>
+                <CardDescription>
+                  Subscribe to recieve uOttawa&apos;s Dining Hall menu everyday
+                  by email at 6:00am.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    value={email}
+                    id="email"
+                    placeholder="user@uottawa.ca"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button
+                  onClick={() => handleSubmit(email)}
+                  className="bg-[#8F001A]"
+                >
+                  Subscribe
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="password">
+            <Card>
+              <CardHeader>
+                <CardTitle>Subscribe wth SMS</CardTitle>
+                <CardDescription>
+                  Subscribe to recieve uOttawa&apos;s Dining Hall menu everyday
+                  by SMS at 6:00am.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="phone">Phone Number</Label>
+                  <Input id="phone" type="phone" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button variant="secondary" disabled={true}>
+                  Coming soon...
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+        <div className={`m-2 ${cn("text-[0.8rem] text-muted-foreground")}`}>
+          Want to unsubscribe? Click{" "}
+          <Link href="/unsubscribe" className="underline">
+            here
+          </Link>
+          .
+        </div>
+      </main>
+    </>
   );
 }
