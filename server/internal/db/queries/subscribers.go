a4d7314bc4aede1d3019f1405e9cc8e03e37d01d
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-08-14 12:03:10 +0300

    feat: get all subscribers

--- server/internal/db/queries/subscribers.go
+++ server/internal/db/queries/subscribers.go
@@ -9,6 +9,30 @@ type Subscriber struct {
 	Email string
 }
 
+func GetAllSubscribers() ([]Subscriber, error) {
+	statement := `SELECT email FROM subscribers`
+	rows, err := db.DB.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var subscribers []Subscriber
+	for rows.Next() {
+		var subscriber Subscriber
+		if err := rows.Scan(&subscriber.Email); err != nil {
+			return nil, err
+		}
+		subscribers = append(subscribers, subscriber)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return subscribers, nil
+}
+
 func AddSubscriber(email string) error {
 	statement := `INSERT INTO Subscribers (email) values ($1)`
 	_, err := db.DB.Exec(statement, email)
