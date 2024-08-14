fd396f7dee65f68ac75d1abb33556db1e802ba32
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2024-08-14 15:31:14 +0300

    fix: unsubscribe error

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -14,35 +14,11 @@ func SetupRoutes(app *fiber.App) {
 	// Register new subsciber
 	app.Post("/api/subscribe", subscribeHandler)
 	app.Post("/api/unsubscribe", unsubscribeHandler)
-
-	// Tracks unique visits
-	// app.Get("/api/track-visit", trackVisitHandler)
-
 }
 
 func healthcheckHandler(c *fiber.Ctx) error {
 	return c.SendString("OK")
 }
-
-// Incorporate a faster DB like Redis to do this
-// func trackVisitHandler(c *fiber.Ctx) error {
-// 	// Check if the visitor has a specific cookie
-// 	cookie := c.Cookies("unique_visitor")
-// 	if cookie == "" {
-// 		// Set a new cookie
-// 		newCookie := new(fiber.Cookie)
-// 		newCookie.Name = "unique_visitor"
-// 		newCookie.Value = "1"
-// 		newCookie.Expires = time.Now().Add(24 * time.Hour) // Expires in 1 day
-// 		c.Cookie(newCookie)
-
-// 		// subscriptions.TotalVisits = subscriptions.TotalVisits + 1
-// 	}
-
-// 	// fmt.Println("Tracked visitors. Total: ", subscriptions.TotalVisits)
-
-// 	// return c.SendString(fmt.Sprint(subscriptions.TotalSubscriptions))
-// }
 
 func subscribeHandler(c *fiber.Ctx) error {
 	//Todo check for duplicates
@@ -55,7 +31,11 @@ func subscribeHandler(c *fiber.Ctx) error {
 	}
 	fmt.Printf("Received email to subscribe: %s\n", req.Email)
 
-	queries.AddSubscriber(req.Email)
+	err := queries.AddSubscriber(req.Email)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Email is already subscribed.")
+	}
 
 	return c.SendString("Success")
 }
